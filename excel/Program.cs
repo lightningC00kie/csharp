eb9d2cc7cc2b19abca104f6b12e2ac843d8be697
kareem <kareemhamed24@gmail.com>
2022-12-07 05:53:54 +0100

    commiting before merge with cycle

--- excel/Program.cs
+++ excel/Program.cs
@@ -29,38 +29,50 @@ class Top
         if (ir.sr == null) {
             return;
         }
-                
-        int colCounter = 0;
-        int intChar;
-        string content = "";
-        sheet.entries.Add(new List<Entry>());
-        while((intChar = ir.ReadChar()) != -1) {
-            char c = (char) intChar;
-            if (!InputReader.IsWhiteSpace(c)) {
-                content += c;
-            }
-            else if (c == '\n' || c == '\r') { // starting a new row
-                if (content != "") {
-                    Entry e = new Entry(content, sheet.entries.Count, colCounter++);
-                    sheet.entries[sheet.entries.Count - 1].Add(e);
-                }
-                sheet.entries.Add(new List<Entry>());
-                content = "";
-                colCounter = 0;
-            }
-            else { // adding new entry to row
-                if (content != "") {
-                    Entry e = new Entry(content, sheet.entries.Count, colCounter++);
-                    sheet.entries[sheet.entries.Count - 1].Add(e);
-                    content = "";
-                }
-            }
-        }
 
-
-        if (content != "") {
-            Entry e = new Entry(content, sheet.entries.Count, colCounter++);
-            sheet.entries[sheet.entries.Count - 1].Add(e);
+        // int intChar;
+        // string content = "";
+        // while((intChar = ir.ReadChar()) != -1) {
+        //     char c = (char) intChar;
+        //     if (!InputReader.IsWhiteSpace(c)) {
+        //         content += c;
+        //     }
+        //     else if (c == '\n') { // starting a new row
+        //         if (content != "") {
+        //             Entry e = new Entry(content, sheet.entries.Count, colCounter++);
+        //             sheet.entries[sheet.entries.Count - 1].Add(e);
+        //         }
+        //         sheet.entries.Add(new List<Entry>());
+        //         content = "";
+        //         colCounter = 0;
+        //     }
+        //     else { // adding new entry to row
+        //         if (content != "") {
+        //             Entry e = new Entry(content, sheet.entries.Count, colCounter++);
+        //             sheet.entries[sheet.entries.Count - 1].Add(e);
+        //             content = "";
+        //         }
+        //     }
+        // }
+        // if (content != "") {
+        //     Entry e = new Entry(content, sheet.entries.Count, colCounter++);
+        //     sheet.entries[sheet.entries.Count - 1].Add(e);
+        // }
+        int row = 0;
+        int col = 0;
+        string? line;
+        while ((line = sr.ReadLine()) != null) {
+            if (line == "") {
+                continue;
+            }
+            // WriteLine(line);
+            sheet.entries.Add(new List<Entry>());
+            foreach (string s in System.Text.RegularExpressions.Regex.Split(line, @"\s+")) {
+                WriteLine(s);
+                sheet.entries[row].Add(new Entry(s, row, col++));
+            }
+            col = 0;
+            row++;
         }
 
         if (sheet.entries.Count == 1 && sheet.entries[0].Count == 0) {
@@ -122,36 +134,16 @@ class InputReader
     public static bool IsWhiteSpace(char c) {
         return c == ' ' || c == '\n' || c == '\t' || c == '\r';
     }
+
+    public static string[] SplitRow(string row) {
+        return System.Text.RegularExpressions.Regex.Split(row, @"\s+");
+    }
 }
-
-// class OutputWriter {
-//     StreamWriter sw;
-//     public OutputWriter(string fileName) {
-//         this.sw = new StreamWriter(fileName);
-//     }
-
-//     public void WriteOutput(Sheet s) {
-//         using (sw) {
-//             int i = 0;
-//             foreach (List<Entry> l in s.entries) {
-//                 foreach (Entry e in l) {
-//                     if (e.col == s.entries[i].Count) {
-//                         sw.Write(e.content + '\n');
-//                     }
-//                     else {
-//                         sw.Write(e.content + ' ');
-//                     }
-//                 }
-//                 i++;
-//             }
-//         }
-        
-//     }
-// }
 
 class Sheet
 {
     public List<List<Entry>> entries = new List<List<Entry>>();
+    List<Entry> cycles = new List<Entry>();
     public static int ResolveColName(string colName)
     {
         int counter = 0;
@@ -162,152 +154,120 @@ class Sheet
         return counter - 1;
     }
 
-    public bool EntryExists(string colName, string rowName)
-    {
-        int colNum = ResolveColName(colName);
-        int rowNum = int.Parse(rowName) - 1;
-        if (rowNum < entries.Count && rowNum >= 0)
-        {
-            if (colNum < entries[rowNum].Count && colNum >= 0)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // public Entry GetEntry(string name)
-    // {
-    //     string colName = "";
-    //     string rowName = "";
-    //     return entries[int.Parse(rowName) - 1][ResolveColName(colName)];
-    // }
-
-    public Formula? ParseFormula(Entry e)
+    public Formula? ParseFormula(Entry e, out string message)
     {
         string? op = Formula.ExtractOp(e.content);
         if (op == null) {
+            message = "#MISSOP";
             return null;
         }
-        string[] operands = Formula.ExtractOperands(e.content, op);
+        Entry[]? operands = Formula.ExtractOperands(this, e.content, op);
+        if (operands == null) {
+            message = "#FORMULA";
+            return null;
+        }
         Formula f = new Formula(op, operands);
-
+        message = "";
         return f;
     }
 
-    string EvaluateFormulaHelper(Entry e, Formula f, string operand) {
-        string row; string col;
-        if (CheckNameFormat(operand, out col, out row)) {
-            if (!EntryExists(col, row)) {
-                return "0";
-            }
-            else {
-                if (IsCycle(e, f, row, col)) {
-                    return "#CYCLE";
-                }
-                
-                Entry temp = entries[int.Parse(row) - 1][ResolveColName(col)];
-                string value = EvaluateCell(temp);
-                if (value == "[]") {
-                    return "0";
-                }
-                else if (int.TryParse(value, out _)) {
-                    return value;
-                }
-                else {
-                    return "#ERROR";
-                }
-            }
-        }
-        else {
-            return "#FORMULA";
-        }
-    }
-
-    bool IsCycle(Entry e, Formula f, string row, string col) {
-        Entry operEntry = entries[int.Parse(row)-1][ResolveColName(col)];
-        if (operEntry.IsFormula()) {
-            Formula? f2 = ParseFormula(operEntry);
-            // if (f2 != null) {
-            //     string operRow; string operCol;
-            //     if (!CheckNameFormat(f2.operands[0], out operCol, out operRow)) {
-            //         return false;
-            //     }
-
-            //     if (ResolveColName(operCol) == e.col && int.Parse(operRow) == e.row) {
-            //         return true;
-            //     }
-
-            //     if (!CheckNameFormat(f2.operands[1], out operCol, out operRow)) {
-            //         return false;
-            //     }
-
-            //     if (ResolveColName(operCol) == e.col && int.Parse(operRow) == e.row) {
-            //         return true;
-            //     }
-
-            // }
-            
-        }
+    bool EntryExists(Entry e) {
+        if (e.row < entries.Count && e.col < entries[e.row].Count && e.row >= 0 && entries[e.row].Count >= 0) {
+            return true;
+        } 
         return false;
-    }
-
-    public string EvaluateFormula(Entry e, Formula formula)
-    {
-        int res;
-        int oper1; int oper2;
-
-        string strOper1 = EvaluateFormulaHelper(e, formula, formula.operands[0]);
-        if (!int.TryParse(strOper1, out oper1)) {
-            return strOper1;
-        }
-
-        string strOper2 = EvaluateFormulaHelper(e, formula, formula.operands[1]);
-        if (!int.TryParse(strOper2, out oper2)) {
-            return strOper2;
-        }
-    
-        if (formula.op == "+")
-        {
-            res = oper1 + oper2;
-        }
-        else if (formula.op == "-")
-        {
-            res = oper1 - oper2;
-        }
-        else if (formula.op == "*")
-        {
-            res = oper1 * oper2;
-        }
-        else
-        {
-            if (oper2 == 0) {
-                return "#DIV0";
-            }
-            res = oper1 / oper2;
-        }
-        return res.ToString();
     }
 
     public string EvaluateCell(Entry e)
     {
-        if (!e.IsFormula())
+        if (e.evaluated) {
+            return e.content;
+        }
+        else if (!e.IsFormula())
         {
             if (int.TryParse(e.content, out _) || e.content == "[]") {
-                return e.content;
+                e.evaluated = true;
             }
             else {
-                return "#INVVAL";
+                e.content = "#INVVAL";
+                e.evaluated = true;
             }
+            return e.content;
         }
         else
         {
-            Formula? f = ParseFormula(e);
-            if (f == null) {
-                return "#MISSOP";
+            if (cycles.Contains(e)) {
+                return "#CYCLE";
+            } 
+            else {
+                cycles.Add(e);
             }
-            return EvaluateFormula(e, f);
+
+            string message;
+            Formula? f = ParseFormula(e, out message);
+            if (f == null) {
+                return message;
+            }
+            
+            string operand1; string operand2;
+            
+            if (f.operands[0].evaluated) {
+                operand1 = f.operands[0].content;
+            }
+            else {
+                operand1 = EvaluateCell(f.operands[0]);
+            }
+
+            if (f.operands[1].evaluated) {
+                operand2 = f.operands[1].content;
+            }
+            else {
+                operand2 = EvaluateCell(f.operands[1]);
+            }
+
+            if (!int.TryParse(operand1, out _) && operand1 != "[]") {
+                string res1 = operand1 == "#CYCLE" ? operand1 : "#ERROR";
+                f.operands[0].content = res1;
+                f.operands[0].evaluated = true;
+                return res1;
+            }
+            if (!int.TryParse(operand2, out _) && operand2 != "[]") {
+                string res2 = operand2 == "#CYCLE" ? operand2 : "#ERROR";
+                f.operands[1].content = res2;
+                f.operands[1].evaluated = true;
+                return res2;
+            }
+            cycles = new List<Entry>();
+            operand1 = operand1 == "[]" ? "0" : operand1;
+            operand2 = operand2 == "[]" ? "0" : operand2;
+            e.content = EvaluateFormula(int.Parse(operand1), int.Parse(operand2), f.op!);
+            e.evaluated = true;
+            return e.content;
         }
+    }
+
+    public string EvaluateFormula(int operand1, int operand2, string op) {
+        int res;
+        if (op == "+")
+        {
+            res = operand1 + operand2;
+        }
+        else if (op == "-")
+        {
+            res = operand1 - operand2;
+        }
+        else if (op == "*")
+        {
+            res = operand1 * operand2;
+        }
+        else
+        {
+            if (operand2 == 0) {
+                return "#DIV0";
+            }
+            res = operand1 / operand2;
+        }
+        return res.ToString();
     }
 
     public static bool CheckNameFormat(string name, out string colName, out string rowName)
@@ -356,9 +316,9 @@ class Sheet
 class Formula : Sheet
 {
     public string? op { get; set; }
-    public string[] operands = new string[2];
+    public Entry[] operands = new Entry[2];
 
-    public Formula(string op, string[] operands)
+    public Formula(string op, Entry[] operands)
     {
         this.op = op; this.operands = operands;
     }
@@ -388,9 +348,43 @@ class Formula : Sheet
         return op;
     }
 
-    public static string[] ExtractOperands(string formula, string op)
+    static bool EntryExists(Sheet s, int row, int col) {
+        return row < s.entries.Count && row >= 0 && col < s.entries[row].Count && col >= 0;
+    }
+
+    public static Entry[]? ExtractOperands(Sheet s, string formula, string op)
     {
-        string[] operands = formula.Substring(1).Split(op);
+        string[] strOperands = formula.Substring(1).Split(op);
+        Entry[] operands = new Entry[2];
+        string rowName1; string colName1;
+        string rowName2; string colName2;
+        if(CheckNameFormat(strOperands[0], out colName1, out rowName1)) {
+            int row = int.Parse(rowName1) - 1; 
+            int col = ResolveColName(colName1);
+            if (EntryExists(s, row, col)) {
+                operands[0] = s.entries[row][col];
+            }
+            else {
+                operands[0] = new Entry("0", row, col);
+            }
+        }
+        else {
+            return null;
+        }
+
+        if(CheckNameFormat(strOperands[1], out colName2, out rowName2)) {
+            int row = int.Parse(rowName2) - 1; 
+            int col = ResolveColName(colName2);
+            if (EntryExists(s, row, col)) {
+                operands[1] = s.entries[row][col];
+            }
+            else {
+                operands[1] = new Entry("0", row, col);
+            }
+        }
+        else {
+            return null;
+        }
         return operands;
     }
 }
@@ -400,6 +394,7 @@ class Entry : Sheet
     public string content;
     public int col;
     public int row;
+    public bool evaluated = false;
     public Entry(string content, int row, int col)
     {
         this.content = content; this.row = row; this.col = col;
